7df2989a8a1764d0614b0230fbe89dd939eecc97
NguyenTrung14 <nguyentrung27092006@gmail.com>
2025-08-07 09:17:32 +0700

    Delete dist/Session04_Bai09.js

--- dist/Session04_Bai09.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-(() => {
-    function calculateInvoiceTotal(invoice) {
-        return invoice.orders.reduce((total, item) => {
-            return total + item.orders.items.product.price * invoice.orders.items.quantity;
-        }, 0);
-    }
-})();
